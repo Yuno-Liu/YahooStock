a27f5bbaf19f9e4fcae8e7b07c49980285cc42fd
ski_Yuno <capoo.liu@icloud.com>
2022-01-11 11:20:02 +0800

    add threading

--- stock.py
+++ stock.py
@@ -1,29 +1,38 @@
 import yfinance as yf
 from colorama import init, Fore, Back, Style
 from termcolor import colored
+import threading
+import os
 
-def getStock(stockId):
-    init(autoreset=True)    #  初始化，並且設置顏色設置自動恢復
-    stock = yf.Ticker(stockId)
-    stockList = stock.history(period="2d")
-    closeF = float(round(stockList['Close'][0],2))  # 前日價格
-    closeL = float(round(stockList['Close'][1],2))  # 目前價格
-    Spread = float(round(closeL - closeF))          # 價差
-    fluctuationLimit = float(round((Spread / closeF) * 100,2))  # 漲跌幅
-    # _str =  + str(fluctuationLimit) + " %"
-    if fluctuationLimit == 0 :
-        print(Back.WHITE + Fore.BLACK + "0.00 %")
-    elif fluctuationLimit > 0 :
-        print(Back.RED + Fore.BLACK + str(fluctuationLimit) + " %")
-    else:
-        print(Back.GREEN + Fore.BLACK + str(fluctuationLimit) + " %")    
+clear = lambda: os.system('cls')
 
-while True:
-    print("離開：!exit")
-    try:
-        stockId = input("請輸入代號：")
-        if stockId == "!exit":
-            break
-        getStock(stockId)
-    except:
-        print("")
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    clear()
+    return t
+
+def call():
+    arr = ["00893.tw","00895.tw","2330.tw","00881.tw"]
+    for x in arr:
+        stock = yf.Ticker(x) # 00893  00895
+        # info = stock.info
+        stockList = stock.history(period="2d")
+        closeF = float(round(stockList['Close'][0],2))  # 前日價格
+        closeL = float(round(stockList['Close'][1],2))  # 目前價格
+        High = float(round(stockList['High'][1],2))
+        Low = float(round(stockList['Low'][1],2))
+        Spread = round(round(closeL,2) - round(closeF,2),2)          # 價差
+        fluctuationLimit = float(round((Spread / closeF) * 100,2))  # 漲跌幅
+        if fluctuationLimit > 0:
+            fluctuationLimit = "+" + str(fluctuationLimit)
+        elif fluctuationLimit < 0:
+            fluctuationLimit = "-" + str(abs(fluctuationLimit))
+        fluctuationLimit = str(fluctuationLimit) + " %"
+        # print(info['shortName'])
+        print("ID：" + x + " 當前價：" + str(closeL) + " 開盤價：" + str(closeF) +" 最高價：" + str(High) + " 最低價：" + str(Low) + " 價差：" + str(Spread) + " 漲跌幅：" + str(fluctuationLimit))
+        
+set_interval(call, 10)        
